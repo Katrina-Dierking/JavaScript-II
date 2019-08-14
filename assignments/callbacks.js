05a4ca82f7296747063536a4d966e5c1e86dc05c
Katrina_Dierking <kalynndier@gmail.com>
2019-08-13 21:12:01 -0400

    "still working"

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -9,6 +9,9 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
+  
+    return cb(arr[0]);
+    }
 
   // Potential Solution:
 
@@ -24,26 +27,61 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
+function firstItem(arr, cb) {
+  // firstItem passes the first item of the given array to the callback function.
+return cb(arr[0]);
+}
+firstItem(items, function(first) {
+  console.log(first)
+});
+
+
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, function(length){
+  console.log(length)
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+last(items, function(last){
+console.log(last)
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let sum = x + y;
+  return cb(sum);
 }
+sumNums(items, function(sum){
+  console.log(sum)
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+// multiplyNums multiplies two numbers and passes the result to the callback.
+let total = x * y;
+return cb(total);
 }
+multiplyNums(items, function(total){
+  console.log(total)
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for(let i = 0; i < list.length; i++) {
+    if(list[i] === item) {
+      result = true;
+    }
+    else result = false;
+  }
+  return cb(result);
 }
 
 /* STRETCH PROBLEM */
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,21 +1,75 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function outerFunc() {
+  let name = "dierking";
+  
+  function innerFunc() {
+    console.log("Katrina " + name); 
+  }
+  
+  return innerFunc; 
+}
+ let fullName = outerFunc();
+fullName();
+
+//Second closure. Different layout//
+
+let addTo = function(outer) {
+  let add = function(inner) {
+  
+    return outer + inner; 
+     };
+  return add;
+};
+
+let addSeven= new addTo(7);
+let addFifteen = new addTo(15);
+let addTen = new addTo(10);
+
+console.log(addSeven(7));
+console.log(addFifteen(7));
+console.log(addTen(7));
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function(){
+    count = count + 1;
+    return count;
+  }
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+
+ // Return a function that when invoked increments and returns a counter variable.
+// newCounter(); //1
+// newCounter(); //2
+// newCounter(); //3
+
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+
+  let counter = 0;
+  console.log(counter);
+  let increment = () => counter += 1;
+  let decrement = () => counter -= 1;
+  let resObj = {increment , decrement};
+  
+  return resObj;
+  
 };
+  let foo = counterFactory();
+  
+  console.log(foo.decrement());
+  console.log(foo.increment());
